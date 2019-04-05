714d7bc5eedef85a3c3a1b5272da8027110740eb
dsvargasn <49066384+dsvargasn@users.noreply.github.com>
2019-04-05 11:26:03 -0500

    Add files via upload

---
+++ ejercicio/Ejercicio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio;
+/**
+ *
+ * @author Estudiantes
+ */
+public class Ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Punto: ");
+        Punto p = new Punto();
+        System.out.println("Valor de x: " + p.getX());
+        System.out.println("Valor de y: " + p.getY());
+    }
+    
+}
